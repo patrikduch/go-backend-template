1926af8dce9287ced9d61fb55bb3c300bea0c9a3
patrikduch <duchpatrik@icloud.com>
2022-11-17 19:50:23 +0100

    Definition of hello-world handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,6 +18,8 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
+	http.HandleFunc("/", Hello)
+
 	// start a webserver
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 
